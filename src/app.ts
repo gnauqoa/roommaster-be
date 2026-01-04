777d060ced2323940dace40471dad8959b8ce0b6
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2026-01-04 23:45:46 +0700

    remove prisma seed config

--- src/app.ts
+++ src/app.ts
@@ -81,7 +81,10 @@ app.use(
 // v1 api routes
 app.use('/v1', routes);
 
-// send back a 404 error for any unknown api request
+app.get('/', (req, res) => {
+  res.send('Roommaster API');
+});
+
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
